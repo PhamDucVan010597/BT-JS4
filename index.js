3d82df03626ace3a4e69faa83481a2b1d03c94fe
PhamDucVan010597 <traibim7x@gmail.com>
2023-06-28 15:07:21 +0700

    chỉnh sửa lỗi

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ document.getElementById("tinhbai1").onclick = function () {
     max = n3;
   }
 
-  if (n1 < n2 && n1 < n3) {
+  if (n1 <= n2 && n1 < n3) {
     min = n1;
-  } else if (n2 < n1 && n2 < n3) {
+  } else if (n2 <= n1 && n2 < n3) {
     min = n2;
   } else {
     min = n3;
   }
-  if ((n1 > n2 && n1 < n3) || (n1 > n3 && n1 < n2)) {
+  if ((n1 >= n2 && n1 <= n3) || (n1 >= n3 && n1 <= n2)) {
     mam = n1;
   } else if ((n2 > n3 && n2 < n1) || (n2 > n1 && n2 < n3)) {
     mam = n2;
